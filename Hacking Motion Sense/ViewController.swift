116deff9a889476a72fa647a29d43a10226dd2b2
DADA <nalydadad@gmail.com>
2019-10-29 21:16:12 +0800

    Initial Commit

---
+++ Hacking Motion Sense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hacking Motion Sense
+//
+//  Created by DADA on 2019/10/29.
+//  Copyright © 2019 nalydadad.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
